f407b642e0485f65e5423d4e43cf596735bb906c
Swastika Library <rahul1.shc@gmail.com>
2020-03-04 23:13:17 +0530

    finished cart

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,31 +13,24 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product( title, price, imageUrl, description, null, req.user._id);
-
-  product.save()
-  .then(result=> {
-    console.log('Created Product');
-    res.redirect('/admin/products')
-  }).catch(err => {
-    console.log(err);
-  });
-  
-  
-};
-
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(products=> {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
+  product
+    .save()
+    .then(result => {
+      // console.log(result);
+      console.log('Created Product');
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err=> {
-    console.log(err);
-  });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -47,21 +40,19 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Product.findById(prodId)
-  //Product.findByPk(prodId)
-  .then(product=> {
-    if (!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-  })
-  .catch(err=> {
-    console.log(err);
-  })
+    // Product.findById(prodId)
+    .then(product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -70,31 +61,41 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  
-  const product= new Product(updatedTitle, updatedPrice, updatedImageUrl, updatedDesc, prodId);
-  
-  product.save()
-  .then(result=> {
-    //console.log('Updated Product');
-    res.redirect('/admin/products');
-  })
-  .catch(err=> {//catch handles errors from both promises product.save and findbypk then
-    console.log(err);
-  })
-  
+
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    prodId
+  );
+  product
+    .save()
+    .then(result => {
+      console.log('UPDATED PRODUCT!');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
 
-
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
+};
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.deleteById(prodId)
-  .then(result=> {
-    //console.log(result);
-    res.redirect('/admin/products');
-  })
-  .catch(err=>{
-    console.log(err);
-  });
-  
+    .then(() => {
+      console.log('DESTROYED PRODUCT');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
--- models/user.js
+++ models/user.js
@@ -1,47 +1,128 @@
-const getDb = require('../util/database').getDb;//acquiring a reference of getDb, to call we shall have to use getDb()
 const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
 const ObjectId = mongodb.ObjectId;
 
 class User {
-    constructor (username, email, cart, id){
-        this.name=username;
-        this.email=email;
-        this.cart=cart;
-        this._id=id;
-    }
+  constructor(username, email, cart, id) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
+  }
 
-    save (){
-        const db = getDb();
-        return db.collection('users').insertOne(this);
-    }
+  save() {
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
 
-    static findById(userId){
-        const db = getDb();
-        return db.collection('users').find({_id: ObjectId(userId)}).next();//find returns a cursor next 
-        //returns in this case only one user, findone will not return a cursor
-    }
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
 
-    addToCart(product){
-        const cartProductIndex = this.cart.items.findIndex(cp=>{//if cart product is -1 means it does not exist
-            return cp.productId.toString() ===product._id.toString();
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems
+    };
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => {
+      return i.productId;
+    });
+    return db
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then(products => {
+        return products.map(p => {
+          return {
+            ...p,
+            quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          };
         });
+      });
+  }
 
-        let newQuantity =1;
-        const updatedCartItems = [...this.cart.items];
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      );
+  }
 
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then(products => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name
+          }
+        };
+        return db.collection('orders').insertOne(order);
+      })
+      .then(result => {
+        this.cart = { items: [] };
+        return db
+          .collection('users')
+          .updateOne(
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
 
-        if(cartProductIndex>=0){
-            newQuantity = this.cart.items[cartProductIndex].quantity+1;
-            updatedCartItems[cartProductIndex].quantity =newQuantity;
-        }
-        else {
-            updatedCartItems.push({items: [{productId: new ObjectId(product._id), quantity: newQuantity}]} );
-        }
-        const db = getDb();
-        const updatedCart = {items: updatedCartItems} ;
-        return db.collection('users').updateOne({_id: new ObjectId(this._id)}, {$set: {cart: updatedCart}})
-    }
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .find({ 'user._id': new ObjectId(this._id) })
+      .toArray();
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection('users')
+      .findOne({ _id: new ObjectId(userId) })
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
 
 module.exports = User;
